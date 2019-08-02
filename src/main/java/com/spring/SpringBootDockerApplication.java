0623398e34c75128f34948e3fe831ab3ba424f3e
Omar-Sherif-Sayed <hulkomar21@gmail.com>
2019-08-02 18:41:02 +0200

    add comment

--- src/main/java/com/spring/SpringBootDockerApplication.java
+++ src/main/java/com/spring/SpringBootDockerApplication.java
@@ -3,6 +3,12 @@ package com.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Spring Boot Docker Project
+ * 
+ * @author Omar Sherif Sayed
+ */
+
 @SpringBootApplication
 public class SpringBootDockerApplication {
 
